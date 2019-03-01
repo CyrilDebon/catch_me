1543f74c78d783f936021e00f3972131b4300420
Guillaume Deleuze <deleuzegui@hotmail.com>
2019-03-01 15:38:10 +0100

    Everything work without refresh

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,23 @@
 class FavoritesController < ApplicationController
 
   def create
-    stop = Stop.find(params[:stop_id])
-    fav = stop.favorites.new(user: current_user)
-    fav.save
+    @stop = Stop.find(params[:stop_id])
+    @favorite = @stop.favorites.new(user: current_user)
+    @favorite.save
+
+    respond_to do |format|
+      format.html { redirect_to stop_path(@stop) }
+      format.js # render create.js.erb
+    end
   end
 
   def destroy
     @favorite = Favorite.find(params[:id])
     @favorite.destroy
+
+    respond_to do |format|
+      format.js
+    end
+
   end
 end
