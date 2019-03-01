450eb01cd432124f0badb102430b976172ff980f
CyrilDebon <cyril.debon@gmail.com>
2019-03-01 14:15:29 +0100

    add style and order of the lines

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_134055) do
+ActiveRecord::Schema.define(version: 2019_03_01_104217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2019_02_26_134055) do
     t.string "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "position"
   end
 
   create_table "stops", force: :cascade do |t|
