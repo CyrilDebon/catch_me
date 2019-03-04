786c7413973a0121d030a04d3a2036a2cca28b8d
Guillaume Roche <guillaumeroche24@gmail.com>
2019-03-04 11:08:49 +0100

    added guest User in the app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  # before_action :authenticate_user!
+
+  set_callback :logging_in_user, :before do
+    guest_user.favorites.each do |favorite|
+      favorite.user = current_user
+      favorite.save
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,23 @@ class User < ApplicationRecord
   has_many :favorites
   has_many :stops, through: :favorites
 
+  def test_method
+    byebug
+  end
+
   def favorite(stop)
     favorites.find_by(stop: stop)
   end
 
   def favorited?(stop)
     favorite(stop).present?
+  end
+
+  def has_favorites?
+    favorites.any?
+  end
+
+  def guest?
+    guest
   end
 end
