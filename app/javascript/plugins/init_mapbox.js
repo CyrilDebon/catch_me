bc72dbf498c29c1d22dc907c0f7d499a1b5340a0
CyrilDebon <cyril.debon@gmail.com>
2019-03-05 14:40:09 +0100

    changement de style de la map, +light

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -7,7 +7,7 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     return new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/mapbox/light-v9',
     });
   };
 
